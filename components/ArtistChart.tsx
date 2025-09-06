a2d5fc01e0edb0e8829728274b66f7aca0fee448
bek2173 <bereketawe@gmail.com>
2025-09-06 17:50:00 +0300

    song list

--- components/ArtistChart.tsx
+++ components/ArtistChart.tsx
@@ -16,7 +16,7 @@ export default function ArtistChart({ data }: ArtistChartProps) {
         <CardHeader>
           <CardTitle className="flex items-center gap-2">
             <Users className="h-5 w-5" />
-            Top Artists by Songs
+            Top Artists by Song
           </CardTitle>
         </CardHeader>
         <CardContent>
