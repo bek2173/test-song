e34789ca7832a184ad832b03d9201914d01b8a15
bek2173 <bereketawe@gmail.com>
2025-09-10 17:08:35 +0300

    mongo

--- components/ArtistChart.tsx
+++ components/ArtistChart.tsx
@@ -16,7 +16,7 @@ export default function ArtistChart({ data }: ArtistChartProps) {
         <CardHeader>
           <CardTitle className="flex items-center gap-2">
             <Users className="h-5 w-5" />
-            Top Artists by their Song
+            Top Artists by Song
           </CardTitle>
         </CardHeader>
         <CardContent>
