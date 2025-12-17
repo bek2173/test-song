4a1c9e8e7fe3efb2d1744c42763b91be793f2fd8
bek2173 <bereketawe@gmail.com>
2025-12-17 09:34:20 +0300

    d

--- components/AddSongModal.tsx
+++ components/AddSongModal.tsx
@@ -6,7 +6,7 @@ import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { createSongRequest } from "@/lib/features/songs/songsSlice"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input" 
+import { Input } from "@/components/ui/input"  
 import { Label } from "@/components/ui/label"
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog"
 
