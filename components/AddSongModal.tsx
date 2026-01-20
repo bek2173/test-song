35d38d6380fa909656c9c44b2e181e9f4dd0064f
bek2173 <bereketawe@gmail.com>
2026-01-20 22:41:12 +0300

    final changes

--- components/AddSongModal.tsx
+++ components/AddSongModal.tsx
@@ -10,7 +10,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"      
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog"
 
-interface AddSongModalProps {
+interface AddSongModalProps {   
   isOpen: boolean    
   onClose: () => void  
 }    
